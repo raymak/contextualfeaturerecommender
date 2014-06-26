4df12436b12fe954464d3655a8cf6dc453c5690c
raymak <kamyar.ardekani@gmail.com>
2014-06-25 17:16:12 -0700

    added: tab progression listener (issue #1 resolved)

--- phase1/addon/lib/main.js
+++ phase1/addon/lib/main.js
@@ -3,6 +3,12 @@ var ui = require('sdk/ui');
 var tabs = require("sdk/tabs");
 var notifications = require("sdk/notifications");
 var timers = require("sdk/timers");
+var windows = require("sdk/windows").browserWindows;
+var windowUtils = require("sdk/window/utils");
+var tabsUtils = require("sdk/tabs/utils")
+var {getOwnerWindow, getTabBrowser} = require("sdk/tabs/utils");
+var { modelFor } = require("sdk/view/core");
+var { viewFor } = require("sdk/model/core");
 
 //initialize a toolbar
 var mytoolbar = require("./toolbar");
@@ -31,22 +37,51 @@ var buton = buttons.ActionButton({
 function handleClick(state){
 	//console.log("button clicked!");
 	
-	var frame = mytoolbar.getFrame();
+	//var frame = mytoolbar.getFrame();
 	
 	//frame.postMessage("", frame.url);
 	//frame.postMessage(tabs.activeTab.url, "*");
 
-	 notifications.notify({ title: "TITLE",  text: "TEXT",  data: "DATA",  onClick: function (data) {console.log(data);}});
+	 //notifications.notify({ title: "TITLE",  text: "TEXT",  data: "DATA",  onClick: function (data) {console.log(data);}});
 
 	//mypanel.show();
 
+	checkTabs();
+
 }
 
-tabs.on('activate', function onOpen(tab) {
-    timers.setTimeout(function (){
-  	panel = mypanel.getPanel();
- 	panel.port.emit("message", tab.url);
- 	panel.show();
-  }, 1000);
-});
+// tabs.on('activate', function onOpen(tab) {
+//     timers.setTimeout(function (){
+//   	panel = mypanel.getPanel();
+//  	panel.port.emit("message", tab.url);
+//  	panel.show();
+//   }, 1000);
+// });
 
+
+function checkTabs(){
+
+	// console.log(tabs.activeTab.url);
+	// tabBrowser = getTabBrowser(getOwnerWindow(viewFor(tabs.activeTab)));
+	// tabBrowser.addTab("http://www.yahoo.com");
+
+	recentWindow = windowUtils.getMostRecentBrowserWindow();
+	tabBrowser = getTabBrowser(recentWindow);
+	// tabBrowser.addTab("http://www.yahoo.com");
+	tabBrowser.addTabsProgressListener({onLocationChange: onLocationChange});
+}
+
+function onLocationChange(aBrowser, aWebProgress, aRequest, aLocation) {
+	console.log("URI change");
+
+	// for each (var tab in tabs)  //finding the tab hosting a specific browser //TODO: put in a module
+		tab = tabs.activeTab;
+		if (tabsUtils.getBrowserForTab(tabsUtils.getTabForId(tab.id)) == aBrowser) {
+			timers.setTimeout(function (){
+  			panel = mypanel.getPanel();
+ 			panel.port.emit("message", tab.url);
+ 			panel.show();
+  				}, 500);
+		}
+
+}
