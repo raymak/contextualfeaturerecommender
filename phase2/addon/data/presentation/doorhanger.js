9a14ecc0119700960f855c61e06e60b87c19de49
raymak <kamyar.ardekani@gmail.com>
2015-05-25 05:25:54 -0700

    Revert 69c1f2e..f79e80c
    This rolls back to commit 69c1f2eda8732fa175278f68b92b2df1db3d7bb1.

--- phase2/addon/data/presentation/doorhanger.js
+++ phase2/addon/data/presentation/doorhanger.js
@@ -34,38 +34,11 @@ self.port.on("updateEntry", function(entry, options){
     self.port.emit("mouseleave");
   });
 
-
-  document.getElementById("neg-feedback").addEventListener("click", openNegFeedback);
-
-  let rsTimeout;
-
-  document.getElementById("rationalecontainer").addEventListener("mouseenter", function(e){
-    if (document.getElementById("recommcontainer").classList.contains("invisible"))
-      return;
-
+  document.getElementById("rationaleopener").addEventListener("click", function(e){
     let rs = document.getElementById("rationalesection");
-    if (rs.classList.contains('visible'))
-      clearTimeout(rsTimeout);
-    else
-      expandRationale();
-  });
-
-  document.getElementById("feedback-form").addEventListener("change", function(e){
-    submitFeedback();
-  });
-
-  document.body.addEventListener("mouseleave", function(e){
-    if (document.getElementById("recommcontainer").classList.contains("invisible"))
-      return;
-
-    let rs = document.getElementById("rationalesection");
-    if (rs.classList.contains('visible')){
-      rsTimeout = setTimeout(collapseRationale, 500);
-    }
-  });
-
-  document.getElementById("info-page").addEventListener("click", function(e){
-    self.port.emit("infoPage");
+    rs.classList.add('visible');
+    
+    updatePanelSize();
   });
 
   window.addEventListener("keydown", function(e){
@@ -100,46 +73,5 @@ function changeBodySize(panelSize){
 function updatePanelSize(width, height){
     self.port.emit("resize", {height: height || Number(getComputedStyle(document.body).height.slice(0,-2)),
      width: width || Number(getComputedStyle(document.body).width.slice(0,-2))});
-}
 
-function openNegFeedback(){
-  collapseRationale();
-  document.getElementById("feedbackcontainer").classList.add("visible");
-  document.getElementById("recommcontainer").classList.add("invisible");
-  document.getElementById("top-left-links").classList.add("visible");
-  document.getElementById("interpunct").classList.add("invisible");
-  document.getElementById("info-page").classList.add("invisible");
-  document.getElementById("neg-feedback").classList.add("active");
-  document.getElementById("button-container").classList.add("feedback");
-  document.getElementById("prim-button").classList.add("invisible");
-  document.getElementById("sec-button").classList.add("feedback");
-  document.getElementById("sec-button").innerHTML = "Learn more about Feature Recommender";
-  // document.getElementById("sec-button").addEventListener("click", function(e){
-  //   self.port.emit("moreInfo");
-  // });
-}
-
-function expandRationale(){
-  document.getElementById("rationalesection").classList.add('visible');
-  updatePanelSize();
-}
-
-function collapseRationale(){
-  document.getElementById("rationalesection").addEventListener("transitionend", function hideRationale(e){
-    document.getElementById("rationalesection").removeEventListener("transitionend", hideRationale);
-    document.getElementById("rationalesection").classList.remove('visible');
-    updatePanelSize();
-  });
-
-  document.getElementById("rationalesection").style.opacity = 0;
-}
-
-function submitFeedback(){
-  setTimeout(function(){
-    document.getElementById("feedbackcontainer").classList.remove("visible");
-    document.getElementById("thankscontainer").classList.add("visible");
-    setTimeout(function(){
-      self.port.emit("hide", "feedbacksubmission", true);
-    }, 3000);
-  }, 500);
 }
--- phase2/addon/lib/presentation/doorhanger.js
+++ phase2/addon/lib/presentation/doorhanger.js
@@ -1,12 +1,10 @@
 "use strict";
 
 const {ActionButton} = require("sdk/ui/button/action");
-const {ToggleButton} = require("sdk/ui/button/toggle");
 const {Panel} = require("../panel");
 const {setTimeout, clearTimeout} = require("timers");
 const {PersistentObject} = require("utils");
 const {extractPresentationData} = require("recommendation");
-const tabs = require("sdk/tabs");
 const data = require("sdk/self").data;
 
 const dhDataAddress = "presentation.dhData";
@@ -20,16 +18,20 @@ const FADE_TIME_MS = 2000;
 let panel;
 let button;
 let hideTimeout;
-let buttonState = false;
 
 function init(){
   button = initButton(buttonClick);
   panel = initPanel(button);
 
+  panel.port.on("buttonClicked", function(){});
+  panel.port.on("hide", pHide);
+  panel.port.on("mouseenter", pMouseenter);
+  panel.port.on("mouseleave", pMouseleave);
+  panel.port.on("resize", resize);
 }
 
 function initPanel(button){
-  let nPanel =  Panel({
+  return Panel({
   autosize: true,
   autohide: false,
   focus: false,
@@ -38,31 +40,19 @@ function initPanel(button){
   onShow: onPanelShow,
   onHide: onPanelHide
   });
-
-  nPanel.port.on("buttonClicked", function(){});
-  nPanel.port.on("hide", pHide);
-  nPanel.port.on("mouseenter", pMouseenter);
-  nPanel.port.on("mouseleave", pMouseleave);
-  nPanel.port.on("resize", resize);
-  nPanel.port.on("infoPage", openInfoPage);
-
-  return nPanel;
-
 }
 
 function initButton(clickHandler){
-  return ToggleButton({
+  return ActionButton({
       id: "dh-button",
-      label: "Feature Recommender",
+      label: "Recommend Feature",
       icon: {
           "16": "./ui/icons/lightbulb_bw.png"
       },
-      onClick: clickHandler,
-      onChange: buttonChange
+      onClick: clickHandler
       // onChange: buttonChange
   });
 }
-
 
 function present(aRecommendation){
   dhData.currentRec = aRecommendation;
@@ -72,7 +62,6 @@ function present(aRecommendation){
 }
 
 function updateEntry(){
-  panel = initPanel(button);
   panel.port.emit("updateEntry", extractPresentationData.call(dhData.currentRec, "doorhanger"));
 }
 
@@ -85,61 +74,23 @@ function updateShow(){
     });
   }, 150);
 
+  
 
   clearTimeout(hideTimeout);
   hideTimeout = setTimeout(function(){
     hide(true);
   }, AUTO_FADE_TIME_MS);
-
-  buttonOn();
 } 
 
 function hide(fadeOut){
-
-  clearTimeout(hideTimeout);
-
   if (fadeOut)
     panel.fadeOut();
   else
     panel.hide();
-
-  buttonOff();
-}
-
-function buttonChange(state){
-  if (state.checked){
-    if (!panel.isShowing)
-    updateShow();
-  }
-  else
-    hide(true);
-
 }
 
 function buttonClick(state){
-
-}
-
-function buttonSwitch(state){
-  if (state)
-    buttonOn();
-  else
-    buttonOff();
-
-}
-function buttonOn(){
-  button.icon = "./ui/icons/lightbulb_gr.png";
-  if (buttonState) return;
-  //when sate is changed as below, button.state does not change, it only changes by button.click()
-  button.state("window", {checked: true}); 
-  buttonState = true;
-}
-
-function buttonOff(){
-  button.icon = "./ui/icons/lightbulb_bw.png";
-  if (!buttonState) return;
-  button.state("window", {checked: false});
-  buttonState = false;
+  updateShow();
 }
 
 function onPanelShow(){
@@ -150,9 +101,9 @@ function onPanelHide(){
 
 }
 
-function pHide(reason, fadeOut){
+function pHide(reason){
   console.log(reason);
-  hide(fadeOut);
+  hide();
 }
 
 function pMouseenter(){
@@ -164,10 +115,6 @@ function pMouseleave(){
   hideTimeout = setTimeout(function(){
     hide(true);
   }, FADE_TIME_MS);
-}
-
-function openInfoPage(){
-  tabs.open(data.url("infopage.html"));
 }
 
 function resize(size){
