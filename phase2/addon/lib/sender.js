0782ca9c3ae7a8423fff2aa66f2b7059261faa4a
raymak <kamyar.ardekani@gmail.com>
2016-02-23 00:50:46 -0800

    sender queuing problem fixed

--- phase2/addon/lib/sender.js
+++ phase2/addon/lib/sender.js
@@ -12,16 +12,15 @@ const ss = require("sdk/simple-storage");
 const {pathFor} = require('sdk/system');
 const path = require('sdk/fs/path');
 const file = require('sdk/io/file');
+const {tickCallback} = require('./timer');
 const {prefs} = require("sdk/simple-prefs");
 const { Buffer, TextEncoder, TextDecoder } = require('sdk/io/buffer');
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/osfile.jsm");
 
-const QUOTA = 50;
-
 const REMOTE_URL = "https://testpilot.mozillalabs.com/submit/" + "featurerecommender";
-// const TETS_URL = "http://logs-01.loggly.com/inputs/ac4fee9c-9dc4-4dc9-8a1b-4094253067bb/tag/http/";
-const TEST_URL = "http://requestb.in/uzm38guz";
+const TEST_URL = "http://logs-01.loggly.com/inputs/ac4fee9c-9dc4-4dc9-8a1b-4094253067bb/tag/http/";
+// const TEST_URL = "http://requestb.in/uzm38guz";
 
 const FILE_NAME = "wp-log";
 const PATH_DIR = pathFor("Desk");
@@ -30,35 +29,16 @@ const observerService = Cc["@mozilla.org/observer-service;1"]
                       .getService(Ci.nsIObserverService);
 
 
-let offlineObs;
-
 function init(){
 
 
-    console.log("initializing sender");
+  console.log("initializing sender");
 
-    let topic = "network:offline-status-changed";
-    offlineObs = {
-    observe: function(aEngine, aTopic, aData){
-      if (aTopic == topic){
-        if (aData == "online"){
-            console.log("flushing the http message queue");
-            flush();
-          }
-        else
-            console.log("offline");
-      }
-    },
-    register: function(){
-      Services.obs.addObserver(offlineObs, topic, false);
-    },
-    unregister: function(){
-      Services.obs.removeObserver(offlineObs, topic, false);
-    }
-  }
 
-  offlineObs.register();
-  unload(offlineObs.unregister);
+  tickCallback(function(et, ett){
+    if (Math.floor(ett) % prefs["sender.resend_period"] == 0)
+      flush();
+  });
 
   if (!ss.storage.sender){
     ss.storage.sender = {};
@@ -68,22 +48,40 @@ function init(){
   flush();
 }
 
+function remove(data){
+  try{
+    ss.storage.sender.messages.splice(
+      ss.storage.sender.messages.findIndex(function(elem) elem.number === data.number),
+      1);
+  } catch(e){console.log(e)}
+  prefs["sender.data.queue_size"] = ss.storage.sender.messages.length;
+}
+
 function queue(data){
-  if (ss.storage.sender.messages.length <= QUOTA){
+  if (ss.storage.sender.messages.length <= prefs["sender.queue_quota"]){
     ss.storage.sender.messages.push(data); 
     console.log("http message queued");
   }
   else
-    console.log("warning: http message lost due to limited quota");
+    console.log("warning: http message dropped due to limited quota");
+
+  prefs["sender.data.queue_size"] = ss.storage.sender.messages.length;
 }
 
 function flush(){
   let arr = ss.storage.sender.messages.slice();
 
+  if (arr.length > 0)
+    console.log("flushing " + arr.length + " queued messages...");
+
   ss.storage.sender.messages = [];
 
-  while (arr.length  > 0)
+  while (arr.length  > 0){
+    console.log(arr.length);
     sendToRemote(arr.shift());
+  }
+
+  prefs["sender.data.queue_size"] = ss.storage.sender.messages.length;
 }
 
 function sendToRemote(data){
@@ -91,16 +89,18 @@ function sendToRemote(data){
   function requestCompleted(which, response) {
     console.log("REQUEST COMPLETE", which, response.status);
 
-    if (response.status != 200)
-        queue(data);
+    if (response.status == 200)
+        remove(data);
   }
 
   let fields = data;
+
+  queue(data);
   
   let XmlReq = new request.Request({
       url: TEST_URL,
       headers: {},
-      onComplete: requestCompleted.bind(null, "TP"),
+      onComplete: requestCompleted.bind(null, fields.number),
       content: JSON.stringify(fields),
       contentType: "application/json"
   });
