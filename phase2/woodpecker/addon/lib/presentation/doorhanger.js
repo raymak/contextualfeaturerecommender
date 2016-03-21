3979a3c4b3b4520054e114ba38f1deda68440c61
raymak <kamyar.ardekani@gmail.com>
2016-03-20 17:31:26 -0700

    dh.js reverted

--- phase2/woodpecker/addon/lib/presentation/doorhanger.js
+++ phase2/woodpecker/addon/lib/presentation/doorhanger.js
@@ -5,23 +5,22 @@
 
 "use strict";
 
-
 const {getMostRecentBrowserWindow, isBrowser} = require("sdk/window/utils");
 const {WindowTracker} = require("sdk/deprecated/window-utils");
+const {ActionButton} = require("sdk/ui/button/action");
+const {Panel} = require("../panel");
 const {setTimeout, clearTimeout} = require("sdk/timers");
-const {PersistentObject, wordCount, weightedRandomInt} = require("../utils");
-const {extractPresentationData, extractResponseCommandMap} = require("../recommendation");
+// const {PersistentObject, wordCount, weightedRandomInt} = require("../utils");
 const {prefs} = require("sdk/simple-prefs");
 const tabs = require("sdk/tabs");
 const {data} = require("sdk/self");
 const {merge} = require("sdk/util/object");
-const {dumpUpdateObject, handleCmd, isEnabled} = require("../debug");
-const {logDhReport, logDhPresent} = require("../logger");
-const timer = require("../timer");
-const self = require("../self");
-const featReport = require("../feature-report");
+// const {dumpUpdateObject, handleCmd, isEnabled} = require("../debug");
+// const {logDhReport} = require("../logger");
+// const timer = require("../timer");
+// const self = require("../self");
+// const logger = require("../logger");
 const unload = require("sdk/system/unload").when;
-
 
 const dhDataAddress = "presentation.doorhanger.data";
 
@@ -30,192 +29,124 @@ let dhData; //initialized in init()
 let panel;
 let button;
 let hideTimeout;
-let letReopen = true;
 let wrdCnt; //for auto-adjuting fading time
-let buttonChecked = false;
+let buttonState = false;
 let command;
 let hideWatch;
 let closedwithreason;
+let fbCallback;
 
 function init(){
   console.log("initializing doorhanger");
+  // button = initButton(buttonClick);
   // panel = initPanel(button);
+
+  // dhData = PersistentObject("simplePref", {address: dhDataAddress, updateListener: debug.update});
   
-  console.time("doorhanger init");
+  // let wt = new WindowTracker({
+  //   onTrack: function(window){
+  //     if (!isBrowser(window)) return;
 
-  dhData = PersistentObject("simplePref", {address: dhDataAddress, updateListener: debug.update});
+  //     let f = function(e){
+  //      if (e.key === "Escape")
+  //       pHide("escape");
+  //     };
 
-  debug.init();
-  // debug.update();
-  
-  let wt = new WindowTracker({
-    onTrack: function(window){
-      if (!isBrowser(window)) return;
-
-      let f = function(e){
-       if (e.key === "Escape")
-        pHide("escape");
-      };
-
-      window.addEventListener("keydown", f);
-      unload(function(){window.removeEventListener("keydown", f)});
-    }
-  });
-
-  if (dhData.currentRec && dhData.currentRec.recomm && !dhData.stopped){
-    button = initButton(buttonClick);
-    updateEntry();
-  }
-
-  console.timeEnd("doorhanger init");
-
+  //     window.addEventListener("keydown", f);
+  //     unload(function(){window.removeEventListener("keydown", f)});
+  //   }
+  // });
 
 }
 
 function initPanel(button){
-  let nPanel =  require("../panel").Panel({
+  let nPanel =  Panel({
     autosize: true,
     autohide: false,
     focus: false,
     contentURL: data.url("./presentation/doorhanger.html"),
-    contentScriptFile: data.url("./presentation/doorhanger.js"),
+    contentScriptFile: [data.url("./js/jquery.min.js"), data.url("./presentation/doorhanger.js")],
     onShow: onPanelShow,
     onHide: onPanelHide
   });
 
   nPanel.port.on("log", function(m){console.log(m)});
   nPanel.port.on("hide", pHide);
-  nPanel.port.on("mouseenter", pMouseenter);
-  nPanel.port.on("mouseleave", pMouseleave);
   nPanel.port.on("resize", resize);
   nPanel.port.on("infoPage", openInfoPage);
-  nPanel.port.on("response", response)
-  nPanel.port.on("liketoggle", likeToggle);
-  nPanel.port.on("dontliketoggle", dontLikeToggle);
-  nPanel.port.on("negfb", negFbSubmit);
-  nPanel.port.on("negfbopen", negFbOpen);
-  nPanel.port.on("rationaleopen", rationaleOpen);
+  nPanel.port.on("fbSubmit", fbSubmit);
 
   return nPanel;
 
 }
 
 function initButton(clickHandler){
-  return require("sdk/ui/button/toggle").ToggleButton({
+  return ActionButton({
       id: "dh-button",
-      label: "Feature Recommender",
+      label: "Woodpecker",
       icon: {
           "16": "./ui/icons/lightbulb_bw.png"
       },
       onClick: buttonClick,
       onChange: buttonChange
+      // onChange: buttonChange
   });
 }
 
-function negFbOrder(){
-  if (dhData["neg_fb_order"])
-    return dhData["neg_fb_order"];
 
-  let order = weightedRandomInt([1, 1, 1, 1, 1, 1]);
-
-  return order;
+function fbSubmit(rate){
+  let result = {type: "rate", rate: rate};
+  fbCallback(result);
 }
 
 
-function present(aRecommendation, cmdCallback){
- 
-  if (dhData.count)
-     dhData.count = dhData.count + 1;
+function present(callback){ 
+  // let dhPresentInfo = {id: aRecommendation.id, number: dhData.count};
+  // logger.logDhPresent(dhPresentInfo);
 
-  if (dhData.currentRec && dhData.currentRec.recomm){
-    updateReport();
-    report(); //report the last recommendation
+  if (panel && panel.isShowing){
+    console.log("warning: doorhanger panel already open");
+    return;
   }
 
-  dhData.currentRec = {recomm: aRecommendation, 
-                       state:{like: false, dontlike: false, count: 0, negFbChoice: null, negFbOpened: false},
-                       report:{number: dhData.count || 1, startett: timer.elapsedTotalTime(), durationtt: 0, primbtn: 0, secbtn: 0,
-                       closebtn: 0, autohide: 0, autofade: 0, esc: 0,
-                       closeother: 0, responseclose: 0, bulbbutton: 0, firstclosereason: "", mouseenter: false,
-                       totalopen: 0, firstopen: 0, rationaleopen: 0, infopage: 0, negfbopen: false}
-                      };
-  command = cmdCallback;
-  console.log("showing " + aRecommendation.id);
-
-   if (!dhData.count)
-    dhData.count = 1;
-
-
-  let dhPresentInfo = {id: aRecommendation.id, number: dhData.count};
-  logDhPresent(dhPresentInfo);
+  fbCallback = callback;
   
   updateShow();  
 }
 
-function updateEntry(options){
+function updateEntry(){
 
-  let currentRec = dhData.currentRec;
-
-  let noInit = options && options.noinit;
-
-  if (!button)
-    button = initButton(buttonClick);
-
-  if (!noInit || !panel || currentRec.state.negFbOpened)
-    panel = initPanel(button);
-
-  let entry = extractPresentationData.call(currentRec.recomm, "doorhanger");
-  panel.port.emit("updateEntry", entry, currentRec.state, {negFbOrder: negFbOrder(), os: self.sysInfo.os});
-
-  wrdCnt = wordCount(entry.message);
-
-  return new Promise(function(resolve, reject){
-    panel.port.on("loaded", function(){resolve()});
-  });
-
+  panel = initPanel(button);
+  panel.port.emit("init", {colorMode: prefs['presentation.doorhanger.color_mode']});
 }
 
 function updateShow(options, panelOptions){
- 
+  updateEntry();
 
-  let delay = (options && options.nodelay)? 0: prefs["presentation.doorhanger.panel_show_delay_ms"];
-  let noInit = options && options.noinit;
+  showPanel(150, panelOptions);
+
   let noSchedule = options && options.noschedule;
 
-  updateEntry({noinit: noInit})
-    .then(function() showPanel(delay, panelOptions))
-    .then(function(){
-    if (!noSchedule)
-      scheduleHide(prefs["presentation.doorhanger.autofade_time_ms_flat"]
-                  +prefs["presentation.doorhanger.autofade_time_ms_per_word"]*wrdCnt);
+  if (!noSchedule)
+    scheduleHide(prefs["presentation.doorhanger.autofade_time_ms"]);
 
-    buttonOn();
-    });
+  // buttonOn();
 } 
 
 function scheduleHide(time_ms){
   clearTimeout(hideTimeout);
   hideTimeout = setTimeout(function(){
     pHide("autohide", true);
+    fbCallback({type: "timeout"});
   }, time_ms);
 }
 
 function showPanel(delay_ms, panelOptions){
 
-  closedwithreason = false;
-  //increment count
-  let currRec = dhData.currentRec;
-  let state = currRec.state;
-  merge(state, {count: state.count+1, negFbOpened: false});
-  dhData.currentRec = merge({}, currRec, {state: state});
-
-  return new Promise(function(resolve, reject){
   //delay to make sure the layout has been loaded
   setTimeout(function(){
-    panel.show(merge({position: button}, panelOptions));
-    resolve();
+    panel.show(merge({position: {top: 0, right: 50}}, panelOptions));
   }, delay_ms || 0);
-  });
 
 }
 
@@ -228,19 +159,22 @@ function hidePanel(fadeOut){
   else
     panel.hide();
 
+
+
+}
+
+function buttonChange(state){
+  // if (state.checked){
+  //   if (!panel.isShowing)
+  //    updateShow({noschedule: true}, {autohide: true, focus: true});
+  // }
+  // else
+  //   hidePanel(true);
 }
 
 function buttonClick(state){
 
- if (!buttonChecked){
-    if (!panel.isShowing)
-      if (letReopen)
-       updateShow({noschedule: true, nodelay:true, noinit: true}, {autohide: true, focus: true});
-     else
-      button.state("window", {checked: false}); 
-  }
-  else
-    pHide("bulbbutton", true);
+
 }
 
 function buttonSwitch(state){
@@ -250,23 +184,19 @@ function buttonSwitch(state){
     buttonOff();
 }
 
-function buttonChange(state){
- 
-}
-
 function buttonOn(){
   button.icon = "./ui/icons/lightbulb_gr.png";
-  if (buttonChecked) return;
+  if (buttonState) return;
   //when sate is changed as below, button.state does not change, it only changes by button.click()
   button.state("window", {checked: true}); 
-  buttonChecked = true;
+  buttonState = true;
 }
 
 function buttonOff(){
   button.icon = "./ui/icons/lightbulb_bw.png";
-  if (!buttonChecked) return;
+  if (!buttonState) return;
   button.state("window", {checked: false});
-  buttonChecked = false;
+  buttonState = false;
 }
 
 function onPanelShow(){
@@ -275,212 +205,36 @@ function onPanelShow(){
 
 function onPanelHide(){
 
-  letReopen = false;
-
-  setTimeout(function(){
-    letReopen = true;
-  }, 200);
-
-  buttonOff();
+  // buttonOff();
 
   clearTimeout(hideTimeout);
 
   let showLength = Date.now()-hideWatch;
-
-  let currRec = dhData.currentRec;
-  let report = currRec.report;
-
-  if (currRec.state.count == 1)
-    report.firstopen = showLength;
-
-  require("./../stats").event("dhHide", {collectInstance: true}, 
-    { showlength: showLength,
-      opencount: currRec.state.count,
-      closedwithreason: closedwithreason
-    });
-
-  report.totalopen = report.totalopen + showLength;
-
-  if (!closedwithreason)
-    report.autofade = report.autofade + 1;
-
-  dhData.currentRec = merge(currRec, {report: report});
 }
 
 function pHide(reason, fadeOut){
 
-  if (!panel || !panel.isShowing){
-    console.log("warning: no panel to hide");
-    // require('./../logger').logWarning({type: "panel-hide"});
-    return;
-  }
-
-  let currRec = dhData.currentRec;
-  let report = currRec.report;
-
-  if (currRec.state.count == 1)
-    report.firstclosereason = reason;
-
-  switch(reason){
-    case "escape":
-      report.esc = report.esc + 1;
-      break;
-
-    case "closebutton":
-      report.closebtn = report.closebtn + 1;
-      break;
-
-    case "autohide":
-      report.autohide = report.autohide + 1;
-      break;
-
-    case "response":
-      report.responseclose = report.responseclose + 1;
-      break;
-
-    case "bulbbutton":
-      report.bulbbutton = report.bulbbutton + 1;
-
-    default:
-      report.closeother = report.closeother + 1;   
-  }
-
-  dhData.currentRec = merge(currRec, {report: report});
-
-  console.log(reason);
-
-  closedwithreason = true;
-
   hidePanel(fadeOut);
 }
 
-function pMouseenter(){
-  clearTimeout(hideTimeout);
-
-  let currRec = dhData.currentRec;
-  let report = currRec.report;
-
-  report.mouseenter = true;
-
-  dhData.currentRec = merge(currRec, {report: report});
-}
-
-function pMouseleave(){
-  clearTimeout(hideTimeout);
-
-  scheduleHide(prefs["presentation.doorhanger.exitfade_time_ms_per_word"]*wrdCnt);
-}
 
 function openInfoPage(){
-  command("info");
-
-  let currRec = dhData.currentRec;
-  let report = currRec.report;
-
-  report.infopage = report.infopage + 1;
-
-  dhData.currentRec = merge(currRec, {report: report});
+  tabs.open(data.url("infopage.html"));
 
 
 }
 
 function resize(size){
-  panel.resize(size.width+3, size.height+3);
+  panel.resize(size.width+4, size.height+4);
 }
 
 function response(element, options){
 
-  let currRec = dhData.currentRec;
-  let report = currRec.report;
-
-  if (element === "primaryButton")
-    report.primbtn = report.primbtn + 1;
-
-  if (element === "secondaryButton")
-    report.secbtn = report.secbtn + 1;
-
-  dhData.currentRec = merge(currRec, {report: report});
-
-  let respCmdMap = extractResponseCommandMap.call(dhData.currentRec.recomm, "doorhanger");
-  command(respCmdMap[element]);
 }
 
-function likeToggle(){
-  console.log("liketoggle");
-  let currRec = dhData.currentRec;
-  let state = currRec.state;
-  merge(state, {like: !state.like});
-  dhData.currentRec = merge(currRec, {state: state});
-}
-
-function dontLikeToggle(){
-  console.log("dontliketoggle");
-  let currRec = dhData.currentRec;
-  let state = currRec.state;
-  state.dontlike = !state.dontlike;
-
-  if (!state.dontlike)
-    state.negFbChoice = null;
-
-  dhData.currentRec = merge(currRec, {state: state});
-}
-
-function negFbSubmit(val){
-  console.log("negative feedback submitted: " + val);
-  let currRec = dhData.currentRec;
-  let state = currRec.state;
-  
-  state.negFbChoice =  val;
-}
-
-function negFbOpen(){
-  let currRec = dhData.currentRec;
-  let state = currRec.state;
-
-  state.negFbOpened = true;
-
-  let report = currRec.report;
-  report.negfbopen = true;
-
-  dhData.currentRec = merge(currRec, {state: state}, {report: report});
-
-
-
-}
-
-function rationaleOpen(){
-  let currRec = dhData.currentRec;
-  let report = currRec.report;
-
-  report.rationaleopen = report.rationaleopen + 1;
-
-  dhData.currentRec = merge(currRec, {report: report});
-}
 
 function updateReport(){
-  let currRec = dhData.currentRec;
-  let state = currRec.state;
-  let report = currRec.report;
 
-  let addedInfo = {durationtt: timer.elapsedTotalTime() - currRec.report.startett,
-                   interaction: Boolean(state.like || state.dontlike || (state.count > 1) 
-                   || state.negFbChoice || (report.primbtn > 0) || (report.secbtn > 0)
-                   || (report.closebtn > 0) || (report.esc > 0) || report.mouseenter
-                   || report.rationaleopen || (report.infopage > 0)),
-                   id: currRec.recomm.id
-                    };
-  let info = merge({}, currRec.state, currRec.report, addedInfo);
-
-  //reporting to feature report
-  let featReportRow = {negfbchoice: info.negFbChoice, dontlike: info.dontlike, presnumber: info.number,
-                    interaction: info.interaction, primbtn: (info.primbtn > 0),
-                    secbtn: (info.secbtn > 0), manualopen: (info.count > 1),
-                    response: (info.primbtn > 0 || info.secbtn > 0), rationaleopen: (info.rationaleopen > 0),
-                    firstclosereason: info.firstclosereason, firstopen: info.firstopen, interaction: info.interaction
-                     }
-  featReport.updateRow(currRec.recomm.id, featReportRow);
-
-  dhData.lastReport = info;
 }
 
 function report(){
@@ -491,73 +245,6 @@ function report(){
     console.log("warning: no report to log.");
 }
 
-function stop(){
-  console.log("stopping doorhanger");
-
-  if (dhData.stopped)
-    return;
-
-  if (button)
-    button.destroy();
-
-  dhData.stopped = true;
-
-  if (dhData.currentRec && dhData.currentRec.recomm){
-    updateReport();
-    report(); //report the last recommendation
-  }
-}
-
-const debug = {
-  init: function(){
-    handleCmd(this.parseCmd);
-  },
-  update: function(){
-    if (!isEnabled) return;
-
-    let data = dhData;
-    let updateObj = {count: data.count, currentRecomm: data.currentRec.recomm, state: data.currentRec.state, report: data.currentRec.report};
-    dumpUpdateObject(updateObj, {list: "Presentation: Doorhanger"});
-  },
-  parseCmd: function(cmd){
-    const patt = /([^ ]*) *(.*)/; 
-    let args = patt.exec(cmd);
-
-    let subArgs;
-    
-    if (!args)  //does not match the basic pattern
-      return false;
-
-    let name = args[1];
-    let params = args[2];
-
-    if (name != "dh")
-      return undefined;
-
-    subArgs = patt.exec(params);
-
-    switch(subArgs[1]){
-      case "report":
-
-        switch(subArgs[2]){
-          case "update":
-            updateReport();
-            report(); //report the last recommendation
-          break;
-
-          default:
-            return "warning: incorrect use of the dh report command.";
-        }
-
-        break;
-      default:
-        return undefined;
-    }
-
-    return " ";
-  }
-}
 
 exports.init = init;
-exports.stop = stop;
 exports.present = present;
