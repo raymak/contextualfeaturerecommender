7f95193130b7087a19ba9117fa18d5f8fe3fcf94
raymak <kamyar.ardekani@gmail.com>
2016-07-01 08:10:01 -0700

    infopage improved

---
+++ phase2/addon/data/infopage.js
@@ -0,0 +1,6 @@
+
+window.onload = function(){
+    document.getElementById("remove-click").addEventListener("click", ()=>{
+        document.getElementById("remove-info").classList.add("visible");
+    });
+}
