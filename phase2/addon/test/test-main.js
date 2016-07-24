74f2b20a6f8c5d891100b049896183c9ffd6dadd
Gregg Lind <gregg.lind@gmail.com>
2016-07-24 15:02:19 -0500

    re #275 fix #277 better coverage and unit testing

---
+++ phase2/addon/test/test-main.js
@@ -0,0 +1,8 @@
+//let index = require("lib/index");
+//
+//console.log("testing!");
+//
+//exports['test main'] = function (assert) {
+//  index.main({loadReason: "install"});
+//  assert.pass("okay, it could start up.")
+//}
